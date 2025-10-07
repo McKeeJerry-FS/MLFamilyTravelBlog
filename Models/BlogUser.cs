15f1dbbdf98750cfce115fae8f5ed8ed5dbc83f0
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-10-07 08:37:06 -0400

    Add project files.

---
+++ Models/BlogUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MLFamilyTravelBlog.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters or less than 2 characters.", MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "Last Name cannot be longer than 50 characters or less than 2 characters.", MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+        [NotMapped]
+        public string? FullName { get 
+            {
+                if (!string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName))
+                {
+                    return $"{FirstName} {LastName}";
+                }
+                return null;
+            }
+        }
+    }
+}
