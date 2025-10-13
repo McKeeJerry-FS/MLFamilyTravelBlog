847cac03b6442318191e75695ecfd3aad556f696
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-10-13 10:43:37 -0400

    Added a data utility class

---
+++ Data/DataUtility.cs
@@ -0,0 +1,32 @@
+﻿namespace MLFamilyTravelBlog.Data
+{
+    public class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            // Use this method to get the connection string for your database
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString! : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            // Implement your logic to build the connection string from the database URL
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+
+            var builder = new Npgsql.NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.AbsolutePath.TrimStart('/'),
+                SslMode = Npgsql.SslMode.Prefer,
+                TrustServerCertificate = true
+            };
+            return builder.ConnectionString;
+        }
+    }
+}
