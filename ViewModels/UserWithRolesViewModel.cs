04a59de7b8d92f397fa5636cc39450fd870a99aa
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-10-13 13:40:39 -0400

    Setup author area

---
+++ ViewModels/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MLFamilyTravelBlog.Models;
+
+namespace MLFamilyTravelBlog.ViewModels
+{
+    public class UserWithRolesViewModel
+    {
+        public BlogUser? User { get; set; }
+        public List<string>? Roles { get; set; }
+    }
+}
